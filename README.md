9e620a75b2e5872a79163cecf4e1aefb6dcfc3e4
achrafmoullablad <achrafmoulablad19@gmail.com>
2024-05-18 12:57:36 +0100

    initial commit start backend project

---
+++ README.md
@@ -0,0 +1,7 @@
+<h2>task-manager-service</h2>
+
+<h3>Create task manager service with spring boot and Keycloak</h3>
+
+<p>Class Diagram</p>
+
+<img src="./Captures/diagramme-classe-taskmanager.png" alt="Class Diagram" />
---
+++ src/test/java/com/taskmanager/service/TaskManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
